63783ae0ab241b369a83eebf32ea9ff0ba19b296
babajs <vince.c@mediaslide.com>
2023-08-31 17:51:28 +0530

    secret key and optoons

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const { notFound, secretKeyProtected } = require("./middleware");
 const app = express();
 app.listen(8080, () => console.log("running bwehehehe"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(secretKeyProtected);
 
 app.post("/generate-pdf", async (req, res) => {
   try {
@@ -15,14 +17,45 @@ app.post("/generate-pdf", async (req, res) => {
     });
 
     const page = await browser.newPage();
-    const { html } = req.body;
-
+    const {
+      html,
+      displayHeaderFooter,
+      footerTemplate,
+      format,
+      headerTemplate,
+      height,
+      landscape,
+      margin,
+      omitBackground,
+      pageRanges,
+      path,
+      preferCSSPageSize,
+      printBackground,
+      scale,
+      timeout,
+      width,
+    } = req.body;
+    //
     await page.setContent(html, { waitUntil: "networkidle0" });
 
     // Generate the PDF
     const pdf = await page.pdf({
-      path: "result1211.pdf",
-      format: "A4",
+      ...(displayHeaderFooter && { displayHeaderFooter }),
+      ...(footerTemplate && { footerTemplate }),
+      format: format || "A4",
+      ...(headerTemplate && { headerTemplate }),
+      ...(height && { height }),
+      ...(landscape && { landscape }),
+      ...(margin && { margin }),
+      ...(omitBackground && { omitBackground }),
+      ...(pageRanges && { pageRanges }),
+      ...(path && { path }),
+      ...(preferCSSPageSize && { preferCSSPageSize }),
+      ...(preferCSSPageSize && { preferCSSPageSize }),
+      printBackground: printBackground || true,
+      ...(scale && { scale }),
+      ...(timeout && { timeout }),
+      ...(width && { width }),
     });
 
     await browser.close();
@@ -34,10 +67,11 @@ app.post("/generate-pdf", async (req, res) => {
     );
 
     // Send the PDF as response
-    res.send(pdf);
+    return res.send(pdf);
   } catch (error) {
     console.error(error);
     res.status(500).send("Error generating PDF");
   }
 });
 //
+app.use(notFound);
